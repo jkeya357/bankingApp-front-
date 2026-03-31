7c2178d6678a2ef5fa3ac94978fee1dcdf364fa5
Joao <jkeya357@gmail.com>
2026-03-31 04:25:06 +0200

    fixed the base url for the backend

--- store/api/bankApi.ts
+++ store/api/bankApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logout } from "../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: "http://localhost:4001/api/v1",
+    baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
     credentials: "include",
     prepareHeaders: (headers, {getState}) => {
       const accessToken = (getState() as RootState).auth.accessToken
